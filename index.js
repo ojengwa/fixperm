a8275a8deaf2f6d1bd603be82f45299f355ba5c2
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-10 01:13:05 +0100

    base command template

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
 
+var program = require('commander');
+var pjson = require('./package.json');
+
+
+program
+    .version(pjson.version)
+    .description('Version: ' + pjson.version'. ' +  pjson.description)
+    .usage('[options] app-name')
+    .arguments('<app>')
+    .option('-u, --user [username]', 'User you want to grant permissions to. Defaults to current logged-in user.')
+    .action(function (app) {
+        // body...
+        console.log(app)
+    })
+    .parse(process.argv);
+
