197c875ecc5de1b2b2131b58346fd0f59d4b8016
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-10 01:31:26 +0100

    add travis and coveralls integrations

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ var pjson = require('./package.json');
 
 program
     .version(pjson.version)
-    .description('Version: ' + pjson.version'. ' +  pjson.description)
+    .description('Version: ' + pjson.version + '. ' + pjson.description)
     .usage('[options] app-name')
     .arguments('<app>')
     .option('-u, --user [username]', 'User you want to grant permissions to. Defaults to current logged-in user.')
     .action(function (app) {
-        // body...
-        console.log(app)
+        'use strict';
+
+        console.log(app);
     })
     .parse(process.argv);
 
